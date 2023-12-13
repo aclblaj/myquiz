4e1b7e3d1ab0d8fde41b708003914a5ae22d1f97
aclblaj <costel.aldea@gmail.com>
2023-12-13 13:16:00 +0100

    single test call

--- src/test/java/com/unitbv/myquiz/services/ExportServiceTest.java
+++ src/test/java/com/unitbv/myquiz/services/ExportServiceTest.java
@@ -17,28 +17,9 @@ class ExportServiceTest {
     ExportService exportService;
 
     @Test
-    void writeToFile() {
-        String OUTPUT_FILE = "C:\\work\\_mi\\2024-BD\\inpQ1-001.xml";
-        String CATEGORY = "2024-BD-Q1-01";
-
-        int result = exportService.writeToFile(OUTPUT_FILE, CATEGORY);
-        logger.info("Number of exported questions: " + result);
-        assertNotEquals(0, result);
-    }
-
-    @Test
-    void writeToFileVDBQ1() {
-        String OUTPUT_FILE = "C:\\work\\_mi\\2024-VDB\\inpQ1-01.xml";
-        String CATEGORY = "2024-VBD-Q1-01";
-
-        int result = exportService.writeToFile(OUTPUT_FILE, CATEGORY);
-        logger.info("Number of exported questions: " + result);
-        assertNotEquals(0, result);
-    }
-
     void writeToFileITSecQ1() {
-        String OUTPUT_FILE = "C:\\work\\_mi\\2024-ITSec\\inpQ1\\inpQ1-01.xml";
-        String CATEGORY = "2024-ITSec-Q1-01";
+        String OUTPUT_FILE = "C:\\work\\_mi\\2024-ITSec\\inpQ1-001.xml";
+        String CATEGORY = "2024-ITSec-Q1-001";
 
         int result = exportService.writeToFile(OUTPUT_FILE, CATEGORY);
         logger.info("Number of exported questions: " + result);
