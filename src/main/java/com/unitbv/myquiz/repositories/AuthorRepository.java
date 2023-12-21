e76299f90612a23df1892eaade89a83fb2225434
aclblaj <10652953+aclblaj@users.noreply.github.com>
2023-12-21 11:09:10 +0100

    process file using parallel tasks

--- src/main/java/com/unitbv/myquiz/repositories/AuthorRepository.java
+++ src/main/java/com/unitbv/myquiz/repositories/AuthorRepository.java
@@ -2,8 +2,9 @@ package com.unitbv.myquiz.repositories;
 
 import com.unitbv.myquiz.entities.Author;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long>, CrudRepository<Author, Long> {
 }
--- src/test/java/com/unitbv/myquiz/services/QuestionServiceTest.java
+++ src/test/java/com/unitbv/myquiz/services/QuestionServiceTest.java
@@ -1,11 +1,11 @@
 package com.unitbv.myquiz.services;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
-import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class QuestionServiceTest {
 
-    Logger logger = Logger.getLogger(QuestionServiceTest.class.getName());
+    Logger logger = org.slf4j.LoggerFactory.getLogger(QuestionServiceTest.class);
 
     @Autowired
     QuestionService questionService;
@@ -23,19 +23,22 @@ class QuestionServiceTest {
 
     @Test
     void parseExcelFilesFromFolderITSecQ1() {
+        long startTime = (int) System.currentTimeMillis();
         if (encodingSevice.checkServerEncoding()) return;
         final String XLSX_DIR_WITH_FILES = "C:\\work\\_mi\\2024-BD\\inpQ1\\";
         String folderPath = XLSX_DIR_WITH_FILES;
         File folder = new File(folderPath);
         int result = questionService.parseExcelFilesFromFolder(folder, 0);
-        logger.info("Number of parsed excel files: " + result);
+        logger.info("Number of parsed excel files: {}", result);
+        long endTime = (int) System.currentTimeMillis();
+        logger.info("Execution time: {} ms", (endTime - startTime));
         assertNotEquals(0, result);
     }
 
     @Test
     void getServerEncoding() {
         String result = encodingSevice.getServerEncoding();
-        logger.info("Server encoding: " + result);
+        logger.info("Server encoding: {}", result);
         assertNotNull(result);
     }
 }
