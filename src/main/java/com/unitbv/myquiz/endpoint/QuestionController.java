863ee330ab06e6b157e61cf6d28293cddde82dcf
Costel Aldea <constantin.aldea@bertelsmann.de>
2024-01-12 07:37:14 +0100

    add rest controller

---
+++ src/main/java/com/unitbv/myquiz/endpoint/QuestionController.java
@@ -0,0 +1,46 @@
+package com.unitbv.myquiz.endpoint;
+
+import com.unitbv.myquiz.dto.QuestionDto;
+import com.unitbv.myquiz.entities.Question;
+import com.unitbv.myquiz.services.QuestionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class QuestionController {
+
+    Logger logger = LoggerFactory.getLogger(QuestionController.class.getName());
+
+    QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/question/{authorId}")
+    public List<QuestionDto> getQuestionByAuthorId(@PathVariable Long authorId) {
+        logger.info("getQuestionByAuthorId: {}", authorId);
+        List<Question> questions;
+        List<QuestionDto> questionDtos = new ArrayList<>();
+        if (null !=authorId) {
+            questions = questionService.getQuestionsForAuthorId(authorId);
+            for (Question question : questions) {
+                questionDtos.add(new QuestionDto(question));
+            }
+        }
+        return questionDtos;
+    }
+}
